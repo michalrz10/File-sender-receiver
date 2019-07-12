fb34fcc089720efbe9bb20a021f3da56e48876cf
michalrz10 <37452210+michalrz10@users.noreply.github.com>
2019-07-12 14:10:28 +0200

    Add files via upload

---
+++ sfile_receiver.py
@@ -0,0 +1,66 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+import socket
+import os
+
+def receive():
+	so=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	so.connect(('8.8.8.8',80))
+	hostt=so.getsockname()[0]
+	so.close()
+	os.chdir('/storage/emulated/0/Przeslane')
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(0.1)
+	iii=0
+	host=''
+	for i in range(len(hostt)):
+		if hostt[i]=='.':
+			iii+=1
+		host+=hostt[i]
+		if iii==3:
+			break
+	powo=False
+	for i in range(1,256):
+		try:
+			hos=host+str(i)
+			s.connect((hos,50000))
+			powo=True
+			break
+		except:
+			s.close()
+			s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(0.05)
+			pass
+        
+	if powo:
+		s.settimeout(50)
+		print('Polaczono z '+ hos)
+		data=s.recv(1024)
+		b=int(str(data,'utf-8'))
+		st=[]
+		li=[]
+		for i in range(b):
+			data=s.recv(1024)
+			st.append(str(data,'utf-8'))
+			data=s.recv(1024)
+			li.append(int(str(data,'utf-8')))
+		l=0
+		for i in li:
+			l+=i
+		p=[]
+		while len(p)<l:
+			data=s.recv(1024)
+			p+=data
+		ile=0
+		s.close()
+		for i in range(len(st)):
+			x=open(st[i],'wb')
+			x.write(bytes(p[ile:ile+li[i]+1]))
+			ile+=li[i]
+			x.close()
+		s.close()
+		print('Odebrano!')
+	else:
+		print('Nie uda³o siê po³¹czyæ!')
---
+++ sfile_sender.py
@@ -0,0 +1,40 @@
+import socket
+import os
+from time import sleep
+
+
+def send():
+	os.chdir('C:\\Users\\michalrz\\Desktop\\Do_wyslania')
+	b=os.listdir()
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(('0.0.0.0',50000))
+	s.listen(1)
+	(c,a)=s.accept()
+	print(a)
+	c.send(bytes(str(len(b)),'utf-8'))
+	sleep(0.1)
+	for i in b:
+		try:
+			x=open(i,'rb')
+		except:
+			print('Nie udalo sie wyslac: '+i)
+			continue
+		data=x.read()
+		if len(data)==0: continue
+		c.send(bytes(i,'utf-8'))
+		sleep(0.1)
+		c.send(bytes(str(len(data)),'utf-8'))
+		sleep(0.1)
+		x.close()
+	for i in b:
+		try:
+			x=open(i,'rb')
+		except:
+			print('Nie udalo sie wyslac: '+i)
+			continue
+		data=x.read()
+		if len(data)==0: continue
+		c.send(data)
+		x.close()
+	c.close()
+	print('Zakonczono!')
