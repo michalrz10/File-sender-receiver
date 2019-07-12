0ffc9469f7250fd6d854ca0b6a8b72d7073fe33d
michalrz10 <37452210+michalrz10@users.noreply.github.com>
2019-07-12 15:04:10 +0200

    Update sfile_sender.py

--- sfile_sender.py
+++ sfile_sender.py
@@ -5,7 +5,7 @@ from time import sleep
 
 def send():
 	os.chdir('path')	#path from which all files will be sent
-	b=os.listdir()
+	files=os.listdir()
 	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(('0.0.0.0',50000))
 	s.listen(1)
@@ -13,11 +13,11 @@ def send():
 	print(a)
 	c.send(bytes(str(len(b)),'utf-8'))
 	sleep(0.1)
-	for i in b:
+	for i in files:		#Sending names and bit length
 		try:
 			x=open(i,'rb')
 		except:
-			print('Nie udalo sie wyslac: '+i)
+			print('Cannot send: '+i)
 			continue
 		data=x.read()
 		if len(data)==0: continue
@@ -26,15 +26,15 @@ def send():
 		c.send(bytes(str(len(data)),'utf-8'))
 		sleep(0.1)
 		x.close()
-	for i in b:
+	for i in files:		#Sending files
 		try:
 			x=open(i,'rb')
 		except:
-			print('Nie udalo sie wyslac: '+i)
+			print('Cannot send: '+i)
 			continue
 		data=x.read()
 		if len(data)==0: continue
 		c.send(data)
 		x.close()
 	c.close()
-	print('Zakonczono!')
+	print('Sended!')
